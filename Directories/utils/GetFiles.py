c541540d67b1447c8319c0a55396ffd4c8f0d908
Microsft-Wrd <60941772+MicrosoftWord12@users.noreply.github.com>
2022-06-20 12:35:44 +0000

    GetFiles, added getting cog stuff

--- Directories/utils/GetFiles.py
+++ Directories/utils/GetFiles.py
@@ -4,9 +4,29 @@ from pathlib import Path
 # Should Get config file
 configFile = os.path.join(os.path.dirname, "../../Templates/Config/Config.json")
 
+# Cog Command
+cogCommand = os.path.join(os.path.dirname, "../../Templates/Cogs/TestCommand.py")
+
+# Cog Event
+cogEvent = os.path.join(os.path.dirname, "../../Templates/Cogs/TestEvent.py")
 
 def get_config_file():
     """
     Should Return Config Files with their new places
     """
+    return configFile
+
+def get_cog_command():
+    """
+    Should return Cog Command File Demo
+    """
+    return cogCommand
+
+def get_cog_event():
+    """
+    Should Return the demo cog
+    """
+    pass
+
+
     
