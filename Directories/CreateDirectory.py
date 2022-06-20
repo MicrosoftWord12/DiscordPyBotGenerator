a77bad35a792816291474f37c2dd138eb2461a47
Microsft-Wrd <60941772+MicrosoftWord12@users.noreply.github.com>
2022-06-20 12:35:19 +0000

    CreateDirectory Beautifying

--- Directories/CreateDirectory.py
+++ Directories/CreateDirectory.py
@@ -1,14 +1,23 @@
 import os
 
-def createDirs():
+def create_dirs():
     """
     Creates directories for the project
     """
     os.mkdir("src")
+    print("Src Folder Created")
+
     os.mkdir("./src/Utils")
+    print("Utils Folder Created")
+
     os.mkdir("./src/Cogs")
+    print("Cogs Folder Created")
+
     os.mkdir("./src/Cogs/Events")
+    print("Cogs, Events Folder Created")
+
     os.mkdir("./src/Cogs/Commands")
+    print("Cogs Commands Folder Created")
 
     return True
 
